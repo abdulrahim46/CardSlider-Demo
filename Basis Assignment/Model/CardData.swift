5cc1d8e62a5dbf16e1b01b598cab1367cd5ced0c
Abdul Rahim <abdul7716@gmail.com>
2020-09-26 12:06:22 +0530

    intial commit
    setting up the things

---
+++ Basis Assignment/Model/CardData.swift
@@ -0,0 +1,8 @@
+//
+//  CardData.swift
+//  Basis Assignment
+//
+//  Created by Abdul chaudhary on 26/09/20.
+//
+
+import Foundation
---
+++ Basis Assignment/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basis Assignment
+//
+//  Created by Abdul chaudhary on 26/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
